3f3d6f5d91a35f2d60d9fb74ed4beb215a7db15f
Ankit Kisi <ankitkisi@Ankits-MacBook-Air.local>
2024-06-05 08:12:19 +0545

    router finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
 import Product from "./pages/Product";
@@ -10,6 +10,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:9000";
 
@@ -42,10 +43,7 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
@@ -56,7 +54,7 @@ function App() {
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
           {/* <Route path="countries" element={<p>The list of countries</p>} /> */}
-          <Route path="form" element={<p>The form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,17 +1,21 @@
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useNavigate } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
-  const [searchParams, ssetSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
+  const navigate = useNavigate();
 
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
       <h1>Map</h1>
       <h1>
         Position: {lat}, {lng}
       </h1>
+      <button onClick={() => setSearchParams({ lat: 23, lng: 24 })}>
+        Change pos
+      </button>
     </div>
   );
 }
